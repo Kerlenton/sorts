a146994e88af60ca43be9975b81f5fa608979ae3
Semyon <51188729+Kerlenton@users.noreply.github.com>
2019-08-11 20:10:52 +0400

    Create shell_sort.c

---
+++ shell_sort.c
@@ -0,0 +1,23 @@
+int* sort(int* list, int length)
+{
+	int i, j, gap, temp;
+
+	for (gap = length / 2; gap > 0; gap /= 2)
+		for (i = gap; i < length; i++)
+		{
+			j = i - gap;
+
+			while (j >= 0 && list[j] > list[j + gap])
+			{
+				temp = list[j];
+				list[j] = list[j + gap];
+				list[j + gap] = temp;
+			}
+		}
+}
+
+// Worst case: O(n^2)
+// Average case: depends on the gap  
+// Best case case: O(n log2 n)      
+
+// Not stable, in place
